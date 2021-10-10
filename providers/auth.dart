a4adfea613ec10cd5d6b2e7914279adf4130cb57
Berke Can Pınar <69467096+WosberbonDesu@users.noreply.github.com>
2021-10-10 13:36:33 +0300

    Update auth.dart

--- providers/auth.dart
+++ providers/auth.dart
@@ -29,7 +29,7 @@ class Auth with ChangeNotifier{
   }
 
   Future<void> _authenticate(String email,String password,String urlSegment)async{
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBnU9hkmwhBQsCL8EIJE_izZ3e3K2i-ngE');
+    final url = Uri.parse('IdentityKit');
     try{
       final response = await http.post(
         url,
